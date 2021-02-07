52b7cedf52e086c6e5fc77e4c5306f4352e7e0d5
Arwa Ali <70253170+DigitalVillian@users.noreply.github.com>
2021-02-07 22:25:55 +0300

    Update modifyContent.js

--- modifyContent.js
+++ modifyContent.js
@@ -1,11 +1,27 @@
+
 function displaySignUp(){
     let hidden = document.getElementById('content');
-    let modify = document.getElementById('hide-elements');
+    let changeHeading= document.getElementById('heading-modify');
+    let changeText = document.getElementById('modify2');
     if (hidden.style.display === "none") {
         hidden.style.display = "inline-flex";
     } else {
-
         hidden.style.display = "none";
-        hidden.style.transition = '0.5s';
+        document.getElementById('heading-modify').innerHTML = 'Already have an Account?';
+        changeText.innerHTML = 'SIGN IN';
+        //document.getElementById('sign-up').innerText = 'SIGN IN';
+        //document.getElementById('heading-modify').innerHTML = 'Already have an Account?';
     }
 }
+/*
+document.addEventListener('DOMContentLoaded', function(){
+    changeText();
+});
+*/
+function changeText(){
+    let hidden = document.getElementById('content');
+
+    return '';
+}
+//let modify = document.getElementById('hide-elements');
+
