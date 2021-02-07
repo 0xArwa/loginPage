f78d575b1be114e4e07a777c79bb9efa7f902cb4
Arwa Ali <70253170+DigitalVillian@users.noreply.github.com>
2021-02-08 00:02:20 +0300

    Update modifyContent.js

--- modifyContent.js
+++ modifyContent.js
@@ -1,5 +1,5 @@
 
-function displaySignUp(){
+/*function displaySignUp(){
     let hidden = document.getElementById('content');
     let changeHeading= document.getElementById('heading-modify');
     let changeText = document.getElementById('modify2');
@@ -17,6 +17,50 @@ function displaySignUp(){
 document.addEventListener('DOMContentLoaded', function(){
     changeText();
 });
+*//*
+function changeText(){
+    let hidden = document.getElementById('content');
+
+    return '';
+}*/
+//let modify = document.getElementById('hide-elements');
+//^ the old code i may need it ;)
+
+function displaySignUp(){
+    let hidden = document.getElementById('content');
+    let changeHeading= document.getElementById('heading-modify');
+    let changeText = document.getElementById('modify2');
+    let signUp_signIn = document.getElementById('sign-up');
+    let displayForm = document.getElementById('display-form');
+
+    if (hidden.style.opacity == '0') {
+        hidden.style.transition = "opacity 0.5s ease-in";
+        hidden.style.opacity = '1';
+        hidden.style.display = "inline-flex";
+        displayForm.style.display = "none";
+        changeHeading.innerHTML = 'Welcome to website';
+        changeText.innerHTML = 'New here?';
+        signUp_signIn.innerHTML = 'SIGN UP';
+    } else {
+        hidden.style.transition = "opacity 0.5s ease-out";
+        hidden.style.opacity = '0';
+        //hidden.style.transition = "visibility 0s, opacity 0.5s linear";
+        //hidden.style.display = "none";
+        /*
+        displayForm.style.display = "inline-flex";
+        changeHeading.innerHTML = 'Already have an account?';
+        changeText.innerHTML = '';
+        signUp_signIn.innerHTML = 'SIGN IN';
+        */
+        //add a pull request then add this if the transition doesn't work
+        //then work at the old version by modifying the display = 'none' etc...
+
+    }
+}
+/*
+document.addEventListener('DOMContentLoaded', function(){
+    changeText();
+});
 */
 function changeText(){
     let hidden = document.getElementById('content');
@@ -24,4 +68,5 @@ function changeText(){
     return '';
 }
 //let modify = document.getElementById('hide-elements');
+
 
